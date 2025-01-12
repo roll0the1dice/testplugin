fc9bcb1b4f61b9a414bcfac8d053a08d012cd2b6
YourName <YourEmail>
2025-01-13 02:14:14 +0800

    add ServiceImpl

--- src/test/java/com/example/testplugin/TestpluginApplicationTests.java
+++ src/test/java/com/example/testplugin/TestpluginApplicationTests.java
@@ -8,18 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.testplugin.controller.TestUserServiceImpl;
 import com.example.testplugin.model.TestUser;
 
-// @SpringBootTest
-// class TestpluginApplicationTests {
-// 	@Autowired
-// 	private TestUserServiceImpl myService;	
-// 	@Test
-// 	void contextLoads() {
-// 		TestUser newTestUser = new TestUser();
-// 		newTestUser.setUsername("user123");
-// 		newTestUser.setUserpassword("password123");
-// 		var resulut = myService.registerUser(newTestUser);
-// 		System.out.println(resulut);
-// 		Assertions.assertThat(resulut).isEqualTo("用户注册成功");
-// 	}
+@SpringBootTest
+class TestpluginApplicationTests {
+	@Autowired
+	private TestUserServiceImpl myService;	
+	@Test
+	void contextLoads() {
+		TestUser newTestUser = new TestUser();
+		newTestUser.setUsername("user123");
+		newTestUser.setUserpassword("password123");
+		var resulut = myService.registerUser(newTestUser);
+		System.out.println(resulut);
+		Assertions.assertThat(resulut).isEqualTo("用户注册成功");
+	}
 
-// }
+}
